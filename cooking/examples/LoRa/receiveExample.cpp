68fefce6b1ae91eb457cf20f3366e46ac9620b19
scottiserickson <scottiserickson@gmail.com>
2017-03-26 14:36:10 -0700

    first working iteration of button interrupt. Have found way to kill subprocess that has cpp receiver code running

--- cooking/examples/LoRa/receiveExample.cpp
+++ cooking/examples/LoRa/receiveExample.cpp
@@ -31,60 +31,44 @@ char my_packet[100];
 
 void setup()
 {
-  // Print a start message
-  printf("SX1272 module and Raspberry Pi: receive packets without ACK\n");
-  
   // Power ON the module
   e = sx1272.ON();
-  printf("Setting power ON: state %d\n", e);
   
   // Set transmission mode
   e = sx1272.setMode(4);
-  printf("Setting Mode: state %d\n", e);
   
   // Set header
   e = sx1272.setHeaderON();
-  printf("Setting Header ON: state %d\n", e);
   
   // Select frequency channel
   e = sx1272.setChannel(CH_10_868);
-  printf("Setting Channel: state %d\n", e);
   
   // Set CRC
   e = sx1272.setCRC_ON();
-  printf("Setting CRC ON: state %d\n", e);
   
   // Select output power (Max, High or Low)
   e = sx1272.setPower('H');
-  printf("Setting Power: state %d\n", e);
   
   // Set the node address
   e = sx1272.setNodeAddress(8);
-  printf("Setting Node address: state %d\n", e);
   
   // Print a success message
-  printf("SX1272 successfully configured\n\n");
   delay(1000);
 }
 
 void loop(void)
 {
-  // Receive message
-  e = sx1272.receivePacketTimeout(10000);
-  if ( e == 0 )
-  {
-    printf("Success: Receive packet, state %d\n",e);
-
-    for (unsigned int i = 0; i < sx1272.packet_received.length; i++)
-    {
-      my_packet[i] = (char)sx1272.packet_received.data[i];
-    }
-    printf("Message: %s\n", my_packet);
-    std::cout << my_packet << std::endl;
-  }
-  else {
-    printf("Fail: Receive packet, state %d\n",e);
-  }
+	// Receive message
+  	e = sx1272.receivePacketTimeout(10000);
+  	if ( e == 0 )
+  	{
+    		for (unsigned int i = 0; i < sx1272.packet_received.length; i++)
+    		{
+      			my_packet[i] = (char)sx1272.packet_received.data[i];
+    		}
+    		std::cout << my_packet << std::endl;
+    		exit(0);
+  	}
 }
 
 int main (){
@@ -92,15 +76,14 @@ int main (){
 	while(1){
 		//loop();
 		e = sx1272.receivePacketTimeout(10000);
-		if ( e == 0 )
+  		if ( e == 0 )
   		{
-			for (unsigned int i = 0; i < sx1272.packet_received.length; i++)
+    			for (unsigned int i = 0; i < sx1272.packet_received.length; i++)
     			{
       				my_packet[i] = (char)sx1272.packet_received.data[i];
     			}
-			std::cout << my_packet << std::endl;
-			return (0);
-		}
+    			std::cout << my_packet << std::endl;
+    			return(0);
+  		}
 	}
 }
-
--- cooking/examples/LoRa/runReceiveCode.py
+++ cooking/examples/LoRa/runReceiveCode.py
@@ -1,9 +1,35 @@
 #!/usr/bin/python 
 
+import os
+import RPi.GPIO as GPIO
 from subprocess import check_output, Popen, PIPE
 
-output = check_output(["sudo", "./receiveExample.cpp_exe"], shell=False)
-print "The output is:", output
-# p = Popen(["sudo", "./receiveExample.cpp_exe"], shell=False, stdout=PIPE, stdin=PIPE)
-# output = p.stdout
+global rx_pid
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+def my_callback(channel):
+    print "button 26 pressed"
+    os.system("sudo kill %s" % (proc.pid, ))
+
+GPIO.add_event_detect(26, GPIO.FALLING, callback=my_callback, bouncetime=300)
+
+#output = check_output(["sudo", "./receiveExample.cpp_exe"], shell=False)
+proc = Popen(["sudo", "./receiveExample.cpp_exe"], stdout=PIPE)
+rx_pid = proc.pid
+out, err = proc.communicate()
+
+print "The output is: ", out, type(out)
+
+GPIO.setup(20, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(21, GPIO.OUT, initial=GPIO.LOW)
+    
+if "1" in out:
+    print "1"
+    GPIO.output(20, GPIO.HIGH)
+elif "2" in out:
+    print "2"
+    GPIO.output(21, GPIO.HIGH)
+else:
+    print "Did not turn on the lights"
