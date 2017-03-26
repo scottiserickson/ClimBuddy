8dd39f91971bfddf2a7f097ad2a3cb0e1b268576
scottiserickson <scottiserickson@gmail.com>
2017-03-26 15:46:47 -0700

    Started writing transmit and receive code

--- cooking/examples/LoRa/runTransReceiveCode.py
+++ cooking/examples/LoRa/runTransReceiveCode.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python 
 
 import os
+import time
 import RPi.GPIO as GPIO
 from subprocess import check_output, Popen, PIPE
 
-global rx_pid, out, err
+global out, err, proc
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -13,6 +14,8 @@ GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 def my_callback(self):
     print "button 26 pressed"
     os.system("sudo kill %s" % (rx_pid, ))
+    runTransmitCode("bigmoney")
+    runReceiveCode()
 
 def gpio_button_setup(gpio_input, gpio_output, button_callback):
     GPIO.setup(gpio_input, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -26,28 +29,35 @@ def b1_callback(self):
     send_serial_message(rx_button_1)
 
 # runs receiver code in a subprocess
-def runReceiveCode(self):
+def runReceiveCode():
     proc = Popen(["sudo", "./receiveExample.cpp_exe"], stdout=PIPE)
+    global rx_pid
     rx_pid = proc.pid
+    global out
     out, err = proc.communicate()
 
 # run transmit code in a subprocess
 def runTransmitCode(message):
+    proc = Popen(["sudo", "./transmitMessage.cpp_exe", message], stdout=PIPE)
+    proc.wait()
     
 GPIO.add_event_detect(26, GPIO.FALLING, callback=my_callback, bouncetime=300)
-
-
-
-print "The output is: ", out, type(out)
 
 GPIO.setup(20, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(21, GPIO.OUT, initial=GPIO.LOW)
     
-if "1" in out:
-    print "1"
-    GPIO.output(20, GPIO.HIGH)
-elif "2" in out:
-    print "2"
-    GPIO.output(21, GPIO.HIGH)
-else:
-    print "Did not turn on the lights"
+
+#if "1" in out:
+#    print "1"
+#    GPIO.output(20, GPIO.HIGH)
+#elif "2" in out:
+#    print "2"
+#    GPIO.output(21, GPIO.HIGH)
+#else:
+#    print "Did not turn on the lights"
+
+runReceiveCode()
+
+while True:
+    if "1" in out:
+        print "received 1"
