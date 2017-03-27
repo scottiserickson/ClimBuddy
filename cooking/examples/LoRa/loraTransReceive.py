487aa62ff10eff8c1d13ae4da28f0987197b37fd
scottiserickson <scottiserickson@gmail.com>
2017-03-26 22:02:31 -0700

    Found BCM buttons for CookingHacks module and setup TxRx code in Python

--- cooking/examples/LoRa/loraTransReceive.py
+++ cooking/examples/LoRa/loraTransReceive.py
@@ -6,30 +6,159 @@ import signal
 import RPi.GPIO as GPIO
 from subprocess import check_output, Popen, PIPE
 
+# GLOBALS --------------------------------------------------------
+
 global out, err
 global rx_proc, rx_pid 
-global tx_proc, tx_pid 
+global tx_proc, tx_pid
+global isRunningTransmit
+isRunningTransmit = False
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# CONSTANTS ------------------------------------------------------
 
-def my_callback(self):
-    print "button 26 pressed"
-    os.system("sudo kill -STOP %s" % (rx_pid))
-    runTransmitCode("bigmoney")
-    os.system("sudo kill -CONT %s" % (rx_pid))
-   
+# GPIO button input pins (connected to ArduPi sheild).
+b1_i = 22 
+b2_i = 27 
+b3_i = 18
+b4_i = 4
+b5_i = 25
+b6_i = 24
+
+# initializes green LED constant values
+green_led_1 = 6 # testing
+green_led_2 = 11
+green_led_3 = 12
+green_led_4 = 13
+green_led_5 = 16
+green_led_6 = 17
+
+# initializes red LED constant values
+red_led_1 = 7 # testing
+red_led_2 = 19
+red_led_3 = 20
+red_led_4 = 21
+red_led_5 = 22
+red_led_6 = 23
+
+# global variables for currently lit LEDS
+red_led_lit = red_led_1
+green_led_lit = green_led_1
+
+# FUNCTION DEFINITIONS -------------------------------------------
+
+# turn off warnings and set gpio pin mode to use BCM numbering
+def gpio_general_setup():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+
+# general button setup for input/output and callback
 def gpio_button_setup(gpio_input, gpio_output, button_callback):
     GPIO.setup(gpio_input, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(gpio_output, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.add_event_detect(gpio_input, GPIO.RISING)
-    GPIO.add_event_callback(gpio_input, button_callback)
+    GPIO.add_event_detect(gpio_input, GPIO.RISING, callback=button_callback, bouncetime=1000)
 
+# changes output to HIGH on port_num
+def gpio_output_high(port_num):
+    GPIO.output(port_num, GPIO.HIGH)
+
+# changes output to LOW on port_num
+def gpio_output_low(port_num):
+    GPIO.output(port_num, GPIO.LOW)
+
+# changes lit green LED to led_num 
+def green_led_change(led_num):
+    print "change green led"
+    #global green_led_lit
+    #if green_led_lit == led_num:
+    #    print 'led lit same\n'
+    #else:
+    #    gpio_output_low(green_led_lit)
+    #    gpio_output_high(led_num)
+    #    green_led_lit = led_num
+        
+# button 1 pressed
 def b1_callback(self):
-    "button 26 pressed"
-    green_led_change(green_led_1)
-    send_serial_message(rx_button_1)
+    print "button 1"
+    global isRunningTransmit
+    if isRunningTransmit == False:
+        isRunningTransmit = True
+        green_led_change(green_led_1)
+        os.system("sudo kill -STOP %s" % (rx_pid))
+        runTransmitCode("1")
+        os.system("sudo kill -CONT %s" % (rx_pid))
+        isRunningTransmit = False
+    else:
+        print "RUNNING CODE"
+
+# button 2 pressed
+def b2_callback(self):
+    print "button 2"
+    global isRunningTransmit
+    if isRunningTransmit == False:
+        isRunningTransmit = True
+        green_led_change(green_led_2)
+        os.system("sudo kill -STOP %s" % (rx_pid))
+        runTransmitCode("2")
+        os.system("sudo kill -CONT %s" % (rx_pid))
+        isRunningTransmit = False
+    else:
+        print "RUNNING CODE"
+
+# button 3 pressed
+def b3_callback(self):
+    print "button 3"
+    global isRunningTransmit
+    if isRunningTransmit == False:
+        isRunningTransmit = True
+        green_led_change(green_led_3)
+        os.system("sudo kill -STOP %s" % (rx_pid))
+        runTransmitCode("3")
+        os.system("sudo kill -CONT %s" % (rx_pid))
+        isRunningTransmit = False
+    else:
+        print "RUNNING CODE"
+
+# button 4 pressed
+def b4_callback(self):
+    print "button 4"
+    global isRunningTransmit
+    if isRunningTransmit == False:
+        isRunningTransmit = True
+        green_led_change(green_led_4)
+        os.system("sudo kill -STOP %s" % (rx_pid))
+        runTransmitCode("4")
+        os.system("sudo kill -CONT %s" % (rx_pid))
+        isRunningTransmit = False
+    else:
+        print "RUNNING CODE"
+
+# button 5 pressed
+def b5_callback(self):
+    print "button 5"
+    global isRunningTransmit
+    if isRunningTransmit == False:
+        isRunningTransmit = True
+        green_led_change(green_led_5)
+        os.system("sudo kill -STOP %s" % (rx_pid))
+        runTransmitCode("5")
+        os.system("sudo kill -CONT %s" % (rx_pid))
+        isRunningTransmit = False
+    else:
+        print "RUNNING CODE"
+
+# button 6 pressed
+def b6_callback(self):
+    print "button 6"
+    global isRunningTransmit
+    if isRunningTransmit == False:
+        isRunningTransmit = True
+        green_led_change(green_led_6)
+        os.system("sudo kill -STOP %s" % (rx_pid))
+        runTransmitCode("6")
+        os.system("sudo kill -CONT %s" % (rx_pid))
+        isRunningTransmit = False
+    else:
+        print "RUNNING CODE"
 
 # runs receiver code in a subprocess
 def runReceiveCode():
@@ -46,21 +175,73 @@ def runTransmitCode(message):
     tx_pid = tx_proc.pid
     # Wait for execution of transmit to complete.
     tx_proc.wait()
-    
-GPIO.add_event_detect(26, GPIO.FALLING, callback=my_callback, bouncetime=300)
 
-GPIO.setup(20, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(21, GPIO.OUT, initial=GPIO.LOW)
+# setup GPIO button type and warnings
+gpio_general_setup()
+
+# setup GPIO buttons
+gpio_button_setup(b1_i, green_led_1, b1_callback)
+gpio_button_setup(b2_i, green_led_2, b2_callback)
+gpio_button_setup(b3_i, green_led_3, b3_callback)
+gpio_button_setup(b4_i, green_led_4, b4_callback)
+gpio_button_setup(b5_i, green_led_5, b5_callback)
+gpio_button_setup(b6_i, green_led_6, b6_callback)
 
 runReceiveCode()
 
 while True:
     out, err = rx_proc.communicate()    
     
-    if "1" in out:
-        print "1!"
+    #if "1" in out:
+    #    print "1!"
         
     runReceiveCode()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
